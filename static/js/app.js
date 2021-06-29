6530f8d4a4eef8f997fd5a6ee9e8e90fe423f293
mpario <79463549+mpario@users.noreply.github.com>
2021-06-28 18:47:34 -0700

    app.js chart created. working on filterby function.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,32 @@
 // from data.js
 var tableData = data;
+// console.log(tableData);
 
-// YOUR CODE HERE!
+//define function to build the table
+function buildTable(dataRows) {
+	var ufoTable = d3.select("#ufo-table")
+	var tbody = d3.select("#ufo-table > tbody")
+
+	tableData.forEach(item => {
+		var row = tbody.append("tr")
+		Object.entries(item).forEach(([key, value]) => {
+			row.append("td").text(value);
+		})
+	})	
+}
+
+//call the function
+buildTable(tableData);
+              
+//allow users to filter
+var button = d3.select("#filter-btn")
+var inputField = d3.select("#datetime")
+
+function handleClick(enterDate) {
+	//filter by date
+	var filteredDate = d3.event.target.value;
+	buildTable(filteredDate)
+	// console.log(filteredDate)
+}
+
+button.on("click", handleClick);
