1cff54e80b99be4a40496993001012ff0047774c
mpario <79463549+mpario@users.noreply.github.com>
2021-06-29 16:41:21 -0700

    app.js updated with filter function

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,11 @@
 var tableData = data;
 // console.log(tableData);
 
+var ufoTable = d3.select("#ufo-table")
+var tbody = d3.select("#ufo-table > tbody")
+
 //define function to build the table
 function buildTable(dataRows) {
-	var ufoTable = d3.select("#ufo-table")
-	var tbody = d3.select("#ufo-table > tbody")
 
 	tableData.forEach(item => {
 		var row = tbody.append("tr")
@@ -13,20 +14,40 @@ function buildTable(dataRows) {
 			row.append("td").text(value);
 		})
 	})	
-}
+};
 
 //call the function
-buildTable(tableData);
-              
-//allow users to filter
+buildTable(tableData);              
+
+//select buttons
 var button = d3.select("#filter-btn")
-var inputField = d3.select("#datetime")
+var form = d3.select(".form-group")
 
-function handleClick(enterDate) {
-	//filter by date
-	var filteredDate = d3.event.target.value;
-	buildTable(filteredDate)
-	// console.log(filteredDate)
-}
+//event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
 
-button.on("click", handleClick);
+//allow users to filter
+function runEnter() {
+	//prevent page from refreshing
+	// d3.event.preventDefault();
+	//select the input element and get the raw HTML node
+	var inputElement = d3.select("#datetime");
+	var inputValue = inputElement.property("value");
+
+	console.log(inputValue);
+	console.log(tableData);
+
+	var filteredData = tableData.filter(dateEntry => dateEntry.datetime === inputValue);
+
+	console.log(filteredData);
+	
+	filteredData.forEach(item => {
+		var row = tbody.append("tr")
+		Object.entries(item).forEach(([key, value]) => {
+			row.append("td").text(value);
+		})
+	})
+
+	buildTable(filteredData);		
+};
